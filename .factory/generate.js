f6d088992fb52abb242c221d1a00fe98657b7585
Nicolas "Norswap" Laurent <norswap@gmail.com>
2018-08-26 16:37:19 +0200

    refactor and making async (except ribosome) - no perf changes

--- .factory/generate.js
+++ .factory/generate.js
@@ -1,100 +1,112 @@
-var marked      = require('marked');
-var matter      = require('gray-matter');
-var moment      = require('moment');
-var escape_html = require("escape-html");
-var tmp         = require('tmp');
-var fs          = require('fs');
+const marked      = require('marked');
+const matter      = require('gray-matter');
+const moment      = require('moment');
+const escape_html = require("escape-html");
+const tmp         = require('tmp-promise');
+const fsp         = require('fs').promises;
 
-function range(n) {
-    return Array(n).fill().map((_, i) => i + 1);
-}
+// TODO: remove looking for work from about
 
-function Post (date, permalink, meta, content) {
+function Post (date, filename, permalink, meta, content) {
     this.date = date
+    this.filename = filename
     this.permalink = permalink
     this.meta = meta
     this.content = content
 }
 
-var url = "http://norswap.com";
-var posts = [];
+const url   = "http://norswap.com";
+const posts = [];
 
 // --- Generates Posts, and fill `posts`
 
-var layout = "post";
-for (filename of fs.readdirSync("posts")) {
-    if (filename[0] == '.') continue;
-    var str = fs.readFileSync("posts/" + filename, {encoding: "utf-8"});
+async function generate_page (type, filename) // relative filename
+{
     var date = moment(filename.slice(0, 10), "YYYY-MM-DD");
-    var permalink = filename.slice(13, -3);
+    var layout = type == "draft"
+        ? "post"
+        : type;
+    var permalink = type == "post"
+        ? filename.slice(13, -3) // strip date, number and extension
+        : filename.slice(0, -3); // strip extension
+    if (type == "draft")
+        permalink = "draft-" + permalink;
+    var src_loc = type + "s/" + filename;
+    var str = await fsp.readFile(src_loc, {encoding: "utf-8"});
     var obj = matter(str);
     var meta = obj.data;
     if (meta.title) meta.title = escape_html(meta.title);
     var content = marked(obj.content);
-    posts.push(new Post(date, permalink, meta, content));
-    fs.mkdirSync("../" + permalink);
-./!output("../" + permalink + "/index.html")
-./!include("page.dna")
+    if (type == "post")
+        posts.push(new Post(date, filename, permalink, meta, content));
+    await fsp.mkdir("../" + permalink);
+.   /!output("../" + permalink + "/index.html")
+.   /!include("page.dna")
 }
 
-// --- Generate Index Pages
+async function generate_pages (type)
+{
+    const promises = [];
+    for (filename of await fsp.readdir(type + "s"))
+        if (filename[0] == '.') continue;
+        else promises.push(generate_page(type, filename));
+    return Promise.all(promises);
+}
 
-posts.reverse();
-var posts_per_page = 15;
-var num_pages = Math.floor((posts.length - 1) / posts_per_page) + 1;
-var layout = "home"
+const posts_promise = generate_pages("post");
 
-for (page of range(num_pages)) {
-    var tmpFile = tmp.tmpNameSync();
-./!output(tmpFile)
-./!include("home.dna")
+// --- Generate Index Pages & Atom Feed
+
+function range(n) {
+    return Array(n).fill().map((_, i) => i + 1);
+}
+
+const posts_per_page = 15;
+
+async function generate_index_page (page, num_pages)
+{
+    var layout = "home";
+    var tmp_file = await tmp.tmpName();
+.   /!output(tmp_file)
+.   /!include("home.dna")
+.   /!stdout() // forces the temp file to be flushed
+    var content = await fsp.readFile(tmp_file, {encoding: "utf-8"});
 
     if (page == 1) {
-./!output("../index.html")
+.       /!output("../index.html")
     } else {
-        fs.mkdirSync("../" + page);
-./!output("../" + page + "/index.html")
+        await fsp.mkdir("../" + page);
+.       /!output("../" + page + "/index.html")
     }
 
-    var content = fs.readFileSync(tmpFile, {encoding: "utf-8"});
-
-./!include("page.dna")
+    var meta = { title: "" + page } // NOT USED
+.   /!include("page.dna")
 }
 
-// --- Generate atom.xml
+async function generate_index_pages_and_atom()
+{
+    await posts_promise;
+    // sort in descending filename order (newest (biggest date) first)
+    posts.sort((a, b) => a.filename > b.filename ? -1 : 1);
 
-./!output("../atom.xml")
-./!include("atom.dna")
+    var num_pages = Math.floor((posts.length - 1) / posts_per_page) + 1;
 
-// --- Generate Pages
+    for (page of range(num_pages))
+        generate_index_page(page, num_pages);
 
-var layout = "page";
-for (filename of fs.readdirSync("pages")) {
-    if (filename[0] == '.') continue;
-    var str = fs.readFileSync("pages/" + filename, {encoding: "utf-8"});
-    var permalink = filename.slice(0, -3);
-    var obj = matter(str);
-    var meta = obj.data;
-    if (meta.title) meta.title = escape_html(meta.title);
-    var content = marked(obj.content);
-    fs.mkdirSync("../" + permalink);
-./!output("../" + permalink + "/index.html")
-./!include("page.dna")
+.   /!output("../atom.xml")
+.   /!include("atom.dna")
 }
 
-// --- Generate Draft Pages
+generate_index_pages_and_atom();
 
-var layout = "post";
-for (filename of fs.readdirSync("draft")) {
-    if (filename[0] == '.') continue;
-    var str = fs.readFileSync("draft/" + filename, {encoding: "utf-8"});
-    var date = moment();
-    var permalink = filename.slice(0, -3);
-    var obj = matter(str);
-    var meta = obj.data;
-    if (meta.title) meta.title = escape_html(meta.title);
-    var content = marked(obj.content);
-    fs.mkdirSync("../draft-" + permalink);
-./!output("../draft-" + permalink + "/index.html")
-./!include("page.dna")
-}
+// --- Generate Pages & Drafts
+
+generate_pages("page");
+generate_pages("draft");
+
+// --- Forces the last written file to be flushed
+
+process.on('exit', () => {
+.   /!stdout()
+});
